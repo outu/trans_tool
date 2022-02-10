cf0448a19379b63bb48e2e7c27f5660c6e21f380
Archibald <1961395960@qq.com>
2022-02-10 18:00:42 +0800

    实现加载效果，暂未实现图标居中

--- Server/ServerApp/Public/components/Task/CompletedTask.js
+++ Server/ServerApp/Public/components/Task/CompletedTask.js
@@ -5,24 +5,31 @@ export default {
     name: 'App',
     template: `
     <div class="build build-linux-client">
-        <el-table :data="list.slice((currentPage-1)*pagesize, currentPage*pagesize)" stripe stype="width: 100%">
-            <el-table-column prop="id" label="序号" width="200%"></el-table-column>
-            <el-table-column prop="record" label="文件" show-overflow-tooltip></el-table-column>
-            <el-table-column prop="size" label="大小"></el-table-column>
-            <el-table-column prop="state" label="状态"></el-table-column>
-            <el-table-column prop="created_at" label="创建时间"></el-table-column>
-            <el-table-column prop="updated_at" label="完成时间"></el-table-column>
-        </el-table>
-        <el-pagination
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            :current-page="currentPage"
-            :page-sizes="[10, 20, 50, 100]"
-            :page-size="pagesize"
-            layout="total, sizes, prev, pager, next, jumper"
-            :total="count">
-        </el-pagination>
-
+    
+        <div v-if="!loading">
+            <el-table :data="list.slice((currentPage-1)*pagesize, currentPage*pagesize)" stripe stype="width: 100%">
+                <el-table-column prop="id" label="序号" width="200%"></el-table-column>
+                <el-table-column prop="record" label="文件" show-overflow-tooltip></el-table-column>
+                <el-table-column prop="size" label="大小"></el-table-column>
+                <el-table-column prop="state" label="状态"></el-table-column>
+                <el-table-column prop="created_at" label="创建时间"></el-table-column>
+                <el-table-column prop="updated_at" label="完成时间"></el-table-column>
+            </el-table>
+            <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="currentPage"
+                :page-sizes="[10, 20, 50, 100]"
+                :page-size="pagesize"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="count">
+            </el-pagination>
+        </div>
+        
+        <div v-if="!!loading">
+            <img src="../../public/css/image/loading.gif">
+        </div>
+        
         <div v-if="!!buildButtonDisabled" class="spinner-border text-primary mt-2" role="status"></div>
         <!-- Modal -->
         <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -63,6 +70,7 @@ export default {
                 { label: '完成时间', key: 'updated_at' }
             ],
             buildButtonDisabled: false,
+            loading: true,
 
         }
     },
@@ -76,12 +84,14 @@ export default {
                 this.list = response.data.data.list;
                 this.count = response.data.data.count;
                 this.message = response.data.message;
+                this.loading = false;
             })
             .catch((error) => {
                 console.log("CATCH_1", error);
                 this.message = error;
                 $('#exampleModalCenter').modal();
                 this.buildButtonDisabled = false;
+                this.loading = false;
             });
     },
     methods: {
